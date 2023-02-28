3016fee441b9dfdb120a8ae7316d87a843583bcb
mackenziebishop <69914681+mackenziebishop@users.noreply.github.com>
2023-02-28 15:27:38 -0600

    Update NavBar.vue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -6,9 +6,11 @@
                 <img src="public\New-IHSA-Logo.png" class="d-inline-block align-top" alt="IHSA Logo">
             </b-navbar-brand>
             <!-- Links for page names-->
-            <b-nav-item href="#">Home</b-nav-item>
-            <b-nav-item href="#">About</b-nav-item>
-            <b-nav-item href="#">Announcements</b-nav-item>
+            <b-navbar-nav>
+                <b-nav-item href="#">Home</b-nav-item>
+                <b-nav-item href="#">About</b-nav-item>
+                <b-nav-item href="#">Announcements</b-nav-item>
+            </b-navbar-nav>
         </b-navbar>
     </div>
 </template>
