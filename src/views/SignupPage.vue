7b09be38df596a587427633fa4439819eb6663e2
Samuel Gutierrez <sammyg1932@gmail.com>
2023-03-01 12:17:14 -0600

    Created signup page and updated background
    made a signup page and changed background image to png for easier use.

---
+++ src/views/SignupPage.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="hero-image">
+    <div class="card" style="width: 22rem; margin: auto; top: 46px; background-color: lightgray; border-style: solid; border-color: black; border-width: 2px;">
+      <div class="card-body">
+        <h4 class="card-title">Sign-Up</h4>
+        <div class="card" style="width: 18rem; margin: auto;">
+          <div class="hello">
+            <img alt="IHSA logo" src="../assets/ihsalogo.png" height="150" width="200">
+            <div>
+              <br>
+              <div class="signup">
+                <h5>
+                Username
+                </h5>
+                <input type="username" id="username" placeholder="Username">
+                <h5>
+                Email
+                </h5>
+                <input type="email" id="email" placeholder="Email">
+                <h5>
+                Password
+                </h5>
+                <input type="password" id="password" placeholder="Password">
+                <h5>
+                Confirm password
+                </h5>
+                <input type="password" id="password" placeholder="Password">
+                <br>
+                <br>
+                <button class="btn btn-dark" type="submit">Signup</button>
+                <p>Have an account? <a href="#">Login here</a> </p>
+              </div>
+            </div> 
+          </div> 
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+
+export default {
+  name: 'SignupPage',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  color: rgb(255, 255, 255);
+  padding: 20px;
+}
+h3 {
+  margin: 40px 0 0;
+  color: rgb(0, 0, 0);
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #4000ff;
+}
+.hero-image{
+  height: 100vh;
+  width: 100%;
+  background-image: url("../assets/ihsabackground.png");
+  background-size: cover;
+  position: absolute;
+  top: 0px;
+  right: 0px;
+  bottom: 0px;
+  left: 0px;
+}
+
+</style>
