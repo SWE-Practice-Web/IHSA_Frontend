9e78563435665564c312acdf28011535e7cab8f4
mackenziebishop <69914681+mackenziebishop@users.noreply.github.com>
2023-03-02 15:11:29 -0600

    Added styles and scripts to event pages

---
+++ src/views/admin/CreateEventPage.vue
@@ -0,0 +1,14 @@
+<template class="createEventContainer">
+
+</template>
+
+<script>
+</script>
+
+<style>
+.createEventContainer{
+    height: 100%;
+    width: 100%;
+    background-color: lightcyan;
+}
+</style>
---
+++ src/views/admin/ManageEventPage.vue
@@ -0,0 +1,14 @@
+<template class="manageEventContainer">
+
+</template>
+
+<script>
+</script>
+
+<style>
+.manageEventContainer{
+    height: 100%;
+    width: 100%;
+    background-color: lightcyan;
+}
+</style>
