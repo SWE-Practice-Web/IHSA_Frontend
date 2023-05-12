faf0efca2b0bd5fed6a68a943f8e373f4e3d0ad9
outwrest <0utWrest@protonmail.com>
2023-05-11 21:53:06 -0500

    updated localhost port for prod deployment

--- src/main.js
+++ src/main.js
@@ -19,7 +19,7 @@ import store from './store'
 const app = createApp(App);
 const axiosInstance = axios.create({
   // baseURL: process.env.VUE_APP_BASE_URL,
-  baseURL: "https://localhost:49177",
+  baseURL: "https://localhost:5001",
   headers: {
     "Content-Type": "application/json",
     "Accept": "application/json",
